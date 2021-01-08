f6684d4356bc72621f55d7ef7bd2afe6e61be484
Jackie <taekwondo.chan@gmail.com>
2021-01-08 11:53:46 -0800

    rebuild embed to houe stock information, now with links

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import html5lib
 from datetime import datetime
 import time
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 client = discord.Client()
 load_dotenv()
@@ -76,15 +78,17 @@ def scrape_benches():
   price = ""
   in_stock_items = {}
   out_of_stock_items = {}
+  links = []
   r = requests.get('https://www.repfitness.com/strength-equipment/strength-training')
   page_content = bs(r.content, features='html5lib')
 
   all_items = page_content.select('li.item.product.product-item')
 
   for item in all_items:
-      item_link = item.select('a.product-item-link')
-      item_name = ("> **" + item_link[0].string.strip() + ":** ")
+      item_link = item.find('a', attrs={'class': 'product-item-link'})
+      item_name = ("**[" + item_link.string.strip() + f"]:** ")
       prices = item.select('div.price-box.price-final_price')
+      links.append(item_link['href'])
 
       try:
           price_from = prices[0].select('p.price-from span.price')
@@ -106,7 +110,7 @@ def scrape_benches():
                       price = (final_price[0].string + " ")
 
       except:
-          price = ("No Price Listed")
+          price = ("No price listed")
 
       add_to_cart = item.select('div.actions-primary')
       in_stock = add_to_cart[0].select('span')
@@ -116,7 +120,7 @@ def scrape_benches():
       elif in_stock[0].string == 'Add to Cart':
           in_stock_items[item_name] = price
 
-  return in_stock_items, out_of_stock_items
+  return in_stock_items, out_of_stock_items, links
 
 def scrape_racks():
   """Scrape Rep's power-racks page"""
@@ -166,6 +170,11 @@ def get_quote():
 @client.event
 async def on_ready():
   print('{0.user} now running'.format(client))
+  print('-------------------')
+  print('Connected Servers:')
+  for guild in client.guilds:
+      print(guild.name)
+  print('-------------------')
 
 @client.event
 async def on_message(message):
@@ -176,6 +185,10 @@ async def on_message(message):
   # Stops the bot from talking to itself
   if message.author == client.user:
     return
+
+  # Respond to ping
+  if client.user.mentioned_in(message) and message.mention_everyone == False:
+    await message.channel.send(':eyes:')
 
   # Testing pinging a user
   if msg_content.startswith('$whoami'):
@@ -237,23 +250,30 @@ async def on_message(message):
   if msg_content.startswith('$benches'):
     message_to_send = ''
     now = datetime.now()
-    in_stock, out_of_stock = scrape_benches()
-    e = discord.Embed(title="Rep Benches", url="https://www.repfitness.com/strength-equipment/strength-training")
-    message_to_send += '\n**IN STOCK**\n'
+    in_stock, out_of_stock, links = scrape_benches()
+
+    message_to_send += '**IN STOCK**\n'
+    i=0
     for item, price in in_stock.items():
+      message_to_send += f':white_check_mark:'
       message_to_send += item
       message_to_send += price
+      message_to_send += f'\n{links[i]}'
       message_to_send += '\n'
+      i+=1
 
-    message_to_send += '\n**OUT OF STOCK**\n'
+    message_to_send += '\n\n**OUT OF STOCK**\n'
     for item, price in out_of_stock.items():
+      message_to_send += ':x:'
       message_to_send += item
       message_to_send += price
       message_to_send += '\n'
-
-    # await message.channel.send(embed=e)
-    await message.channel.send(f'**\nRep Benches:** Updated {now.strftime("%H:%m:%S")} UTC\n\n')
-    await message.channel.send(message_to_send)
+    
+    e = discord.Embed(url="https://www.repfitness.com/strength-equipment/strength-training", description=message_to_send, color=0x23cc50)
+    e.set_author(name='BENCHES', url='https://www.repfitness.com/strength-equipment/strength-training')
+    e.set_thumbnail(url='https://www.repfitness.com/media/catalog/product/cache/6031cf661625f6f6abd8f87ef140b802/w/i/wide-pad.jpg')
+    e.set_footer(text=f'Updated {now.strftime("%H:%m:%S")} UTC', icon_url='https://i.imgur.com/1sqNK27b.jpg')
+    await message.channel.send(embed=e)
 
 # Run run botty boi
 client.run(TOKEN)
